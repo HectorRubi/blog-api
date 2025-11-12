059fe023876aee7cb9f17e8d15fff4e180ec1c24
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-11-12 15:36:51 -0600

    feat: add PostgreSQL support with TypeORM and configuration management
    - Added @nestjs/config and @nestjs/typeorm dependencies to package.json.
    - Configured global configuration module in app.module.ts.
    - Set up TypeORM with PostgreSQL connection using environment variables.
    - Created env.model.ts to define the environment configuration interface.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvConfig } from './env.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<EnvConfig>) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST', { infer: true }),
+        port: configService.get('DATABASE_PORT', { infer: true }),
+        username: configService.get('DATABASE_USERNAME', { infer: true }),
+        password: configService.get('DATABASE_PASSWORD', { infer: true }),
+        database: configService.get('DATABASE_NAME', { infer: true }),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
