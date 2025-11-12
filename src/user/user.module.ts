7337896a933119e69386c00d5ff12a1b87e3f5fb
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-11-12 16:25:14 -0600

    feat: add TypeORM integration for User and Profile entities in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Profile } from './entities/profile.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
