a7f00af9a22396ab1344a8e3eceef5325806f246
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-12-21 16:26:24 -0600

    feat: add AuthController with login endpoint and corresponding tests

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { UserModule } from '../user/user.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UserModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
